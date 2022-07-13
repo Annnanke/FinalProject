8ba00fa574013db233eca4a1387a3b07b2310740
Daniel <stickbugdanny@gmail.com>
2022-07-13 19:58:01 +0300

    user base

--- src/server/main/java/Baiscs/User.java
+++ src/server/main/java/Baiscs/User.java
@@ -1,4 +1,19 @@
 package Baiscs;
 
 public class User {
+    private Integer id;
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public User(Integer id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
 }
