dc2cb4763f8adcc45d22100a9e0ae1832a5344e6
Daniel <stickbugdanny@gmail.com>
2022-07-13 20:02:59 +0300

    product constructors ready

--- src/server/main/java/Baiscs/Product.java
+++ src/server/main/java/Baiscs/Product.java
@@ -1,4 +1,43 @@
 package Baiscs;
 
 public class Product {
+    private Integer id;
+    private String name;
+    private String description;
+    private String distibutor;
+    private Double amount;
+    private Double price;
+    private Integer category_id;
+
+
+    /**EVERYTING constructor*/
+    public Product(Integer id, String name, String description, String producer, Double amount, Double price, Integer category_id) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.distibutor = producer;
+        this.amount = amount;
+        this.price = price;
+        this.category_id = category_id;
+    }
+    /**NO ID constructor*/
+    public Product(String name, String description, String producer, Double amount, Double price, Integer category_id) {
+        this.name = name;
+        this.description = description;
+        this.distibutor = producer;
+        this.amount = amount;
+        this.price = price;
+        this.category_id = category_id;
+    }
+
+    /**NO ID NO CATEGORY ID constructor*/
+    public Product(String name, String description, String producer, Double amount, Double price) {
+        this.name = name;
+        this.description = description;
+        this.distibutor = producer;
+        this.amount = amount;
+        this.price = price;
+    }
+
+
 }
--- src/server/main/java/Baiscs/User.java
+++ src/server/main/java/Baiscs/User.java
@@ -16,4 +16,30 @@ public class User {
         this.password = password;
     }
 
+
+
+//WATCH OUT
+    public Integer getId() {
+    return id;
+}
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
