c91274dfe1cf8db37ab638552e823529175de3e3
Annnanke <kirikanna3005@gmail.com>
2022-07-13 13:42:57 -0400

    Requesters remastered

---
+++ src/client/main/java/Requesters/DeleteRequests.java
@@ -0,0 +1,4 @@
+package Requesters;
+
+public class DeleteRequests {
+}
--- src/client/main/java/Requesters/PutRequests.java
+++ src/client/main/java/Requesters/PutRequests.java
@@ -23,17 +23,50 @@ public class putRequests {
     public static int[] putProductRequest(Product product) {
         String body = null;
         product.setId(1);
-       body = objectMapper.writeValueAsString(product);
-
+        try {
+            body = objectMapper.writeValueAsString(product);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return putSample(LOCAL_URL_PRODUCT, body);
     }
 
-    public static int[] putCategoryRequest(Category category) {
+    public static int[] putCategoryRequest(    Category category) {
         String body = null;
-
+        try {
+            body = objectMapper.writeValueAsString(category);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return putSample(LOCAL_URL_CATEGORY, body);
     }
 
     private static int[] putSample(String path, String body){
-
+        HttpRequest request = null;
+        try {
+            request = HttpRequest.newBuilder()
+                    .uri(new URI(path))
+                    .header("accept", "application/json")
+                    .header("Authorization", AUTH_TOKEN)
+                    .PUT(HttpRequest.BodyPublishers.ofString(body))
+                    .build();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
 
+        HttpResponse<String> response = null;
+        int id = 0;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() == 201) {
+                byte[] array = response.body().getBytes();
+                ByteBuffer wrapped = ByteBuffer.wrap(array); // big-endian by default
+                id = wrapped.getInt();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return new int[]{response.statusCode(), id};
+    }
 }
