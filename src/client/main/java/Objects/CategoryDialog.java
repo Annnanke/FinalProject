012e3d23235ada4e0f85ed4a45c3fd05df1f9ecc
Annnanke <kirikanna3005@gmail.com>
2022-07-12 23:22:03 -0400

    JavaFX connected 16 versions

---
+++ src/client/main/java/Objects/CategoryDialog.java
@@ -0,0 +1,18 @@
+package Objects;
+
+
+import java.awt.*;
+
+public class CategoryDialog extends Dialog<Category> {
+
+    // For new items
+    public CategoryDialog () {
+        super();
+        return null;
+    }
+
+    // For edits
+    public CategoryDialog (Category category) {
+        super();
+
+}
---
+++ src/client/main/java/Objects/ProductDialog.java
@@ -0,0 +1,17 @@
+package Objects;
+
+
+public class ProductDialog extends Dialog<Product> {
+
+    // For new items
+    public ProductDialog (int categoryId) {
+        super();
+        return null;
+
+    }
+
+    // For edits
+    public ProductDialog (int categoryId, Product product) {
+        super();
+        return null;
+}
