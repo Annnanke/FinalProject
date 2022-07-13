a12d9bb875607e88cb607b25daf76825952fab10
Annnanke <kirikanna3005@gmail.com>
2022-07-13 14:06:03 -0400

    налаштування мережі

--- src/client/main/java/Objects/AddSubstractAmount.java
+++ src/client/main/java/Objects/AddSubstractAmount.java
@@ -2,30 +2,9 @@ package Objects;
 
 
 public class AddSubtractAmount extends Dialog<Double> {
-    public AddSubtractAmount(boolean add, Double currentAmount) {
-        super();
-        DialogPane dialogPane = this.getDialogPane();
-        dialogPane.getButtonTypes().addAll(ButtonType.APPLY, ButtonType.CANCEL);
 
-        TextField amount = new TextField();
-
-        dialogPane.setContent(new VBox(5, addLabel, amount));
-
-        this.setResultConverter((ButtonType button) -> {
-            if (button == ButtonType.APPLY) {
-                try {
-                    Double amountValue = Double.parseDouble(amount.getText());
-                    if ((add ? amountValue : -amountValue) + currentAmount < 0)
-                        throw new IllegalArgumentException("amount can't be negative");
-                    return Double.parseDouble(amount.getText());
-                } catch (Exception e) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setContentText("Wrong parameters!\n(" + e.getMessage() + ")");
-                    alert.showAndWait();
-                }
-            }
             return null;
-        });
+
     }
 
 }
